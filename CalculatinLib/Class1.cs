3b3d7b96d3c2f296a583797916b0a45dba5ddfad
Хабибулин Даниил Олегович <khabibulin.do1648@okeit.edu>
2023-04-24 10:29:44 +0300

    Добавьте файлы проекта.

---
+++ CalculatinLib/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace CalculatinLib
+{
+    public static class CalculatoinLib
+    {
+       public static double Plus()
+        {
+            Console.WriteLine("Метод сложение:");
+            Console.Write("Введите a - ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b - ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a + b;
+        }
+        public static double Minus()
+        {
+            Console.WriteLine("Метод вычитание:");
+            Console.Write("Введите a - ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b - ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a - b;
+        }
+       public static double divide()
+        {
+            Console.WriteLine("Метод деление:");
+            Console.Write("Введите a - ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b - ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a / b;
+        }
+       public static double multiply()
+        {
+            Console.WriteLine("Метод умножение:");
+            Console.Write("Введите a - ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите b - ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            return a * b;
+        }
+    }
+}   
---
+++ Calculatorwithlib/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CalculatinLib;
+
+namespace Calculatorwithlib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CalculatinLib.CalculatoinLib.Plus());
+            Console.WriteLine(CalculatinLib.CalculatoinLib.Minus());
+            Console.WriteLine(CalculatinLib.CalculatoinLib.multiply());
+            Console.WriteLine(CalculatinLib.CalculatoinLib.divide());
+        }
+    }
+}
